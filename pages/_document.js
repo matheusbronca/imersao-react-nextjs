79e82e13cea893c29cb77ceb5cb1eb249a8ccbef
Matheus Bronca <dev@matheusbronca.com>
2021-01-27 11:28:12 -0300

    Aula 01 - Adicionadas Meta Tags

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,42 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* <!-- Primary Meta Tags --> */}
+          <title>Quiz The Witcher</title>
+          <meta name="title" content="Quiz The Witcher" />
+          <meta
+            name="description"
+            content="Teste seus conhecimentos sobre a franquia The Witcher
+e se divirta."
+          />
+
+          {/* <!-- Open Graph / Facebook --> */}
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:url"
+            content="https://imersao-react-nextjs.matheusbronca.vercel.app/"
+          />
+          <meta property="og:title" content="Quiz The Witcher" />
+          <meta
+            property="og:description"
+            content="Teste seus conhecimentos sobre a franquia The Witcher
+e se divirta."
+          />
+          <meta property="og:image" content="/images/og-image.jpg" />
+
+          {/* <!-- Twitter --> */}
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://imersao-react-nextjs.matheusbronca.vercel.app/"
+          />
+          <meta property="twitter:title" content="Quiz The Witcher" />
+          <meta
+            property="twitter:description"
+            content="Teste seus conhecimentos sobre a franquia The Witcher
+e se divirta."
+          />
+          <meta property="twitter:image" content="/images/og-image.jpg"></meta>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap"
